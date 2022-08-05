5790f2f407e423c86bfefa41fc82e1b3f4a4c67c
Ibrahim Azez <ibrahimazez@protonmail.com>
2022-08-05 16:36:19 +0300

    ADD: i18n

---
+++ src/app/core/modules/angular-material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@NgModule({
+	declarations: [],
+	imports: [],
+	exports: [
+		MatButtonModule,
+		TextFieldModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatSidenavModule,
+	],
+})
+export class AngularMaterialModule {}
